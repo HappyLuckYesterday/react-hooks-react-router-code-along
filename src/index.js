42d3656f2f449823728462ae46b61dbf513919c9
HappyYama <milesdanieldragon@gmail.com>
2017-02-07 11:56:30 -0500

    updating react router readme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Router, Route, hashHistory } from 'react-router'
+
+const App = () => {
+    return (
+        <div><h1>Our App Component</h1></div>
+    )
+}
+
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="home" component={App} />
+    </Router>
+), document.getElementById('container'))
